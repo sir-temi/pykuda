5fc1c37ca5e5ac42c1979d08e19778d8c834ec27
TemiTope <siryankey@protonmail.com>
2023-11-04 13:37:57 +0000

    Fixed issue with environmental variables

--- src/pykuda/pykuda.py
+++ src/pykuda/pykuda.py
@@ -1,4 +1,5 @@
 from pykuda.classes.service_type import ServiceType
+from pykuda.utils import check_envs_are_set
 
 
 class PyKuda(ServiceType):
@@ -7,9 +8,11 @@ class PyKuda(ServiceType):
     """
 
     def __init__(self):
-        response = self.check_envs_are_set()
+        response = check_envs_are_set()
 
         # If all credentials were properly set, response will
-        # be a bool(True), else will be a string
-        if not isinstance(response, bool):
+        # be a dictionary of credentials, else will be a string
+        if not isinstance(response, dict):
             raise ValueError(response)
+
+        self.credentials = response
