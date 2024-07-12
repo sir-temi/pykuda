eec3c150da9720c893574fc68ee786da0b504e1c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-07-12 10:25:22 +0100

    Update py_kuda_response.py

--- src/pykuda/classes/py_kuda_response.py
+++ src/pykuda/classes/py_kuda_response.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass
 
-
 @dataclass
 class PyKudaResponse:
     """
-    Helps simplify Kuda's response
-    """
+    A class to simplify and standardize the response from Kuda API.
 
+    Attributes:
+        status_code (int): The HTTP status code of the response.
+        data (dict): The data payload of the response.
+        error (bool): Indicates whether there was an error in the response (default is False).
+    """
     status_code: int
     data: dict
     error: bool = False
